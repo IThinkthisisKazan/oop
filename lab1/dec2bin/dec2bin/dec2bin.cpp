e4b855b36a4c2ce784120949a98cd88507d349ee
Ayar Tokoev <56004530+IThinkthisisKazan@users.noreply.github.com>
2020-04-03 22:50:08 +0300

    complete

---
+++ lab1/dec2bin/dec2bin/dec2bin.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include <string>
+using namespace std;
+int main(int argc, char* argv[])
+{
+
+	if (argc != 2)
+	{
+		setlocale(LC_ALL, "Russian");
+		cout << "incorrect input \n" << "need: dec2bin.exe < числов в десятичной системе > \n";
+		return 1;
+	}
+
+	int t = 0, d = 1;
+	string test = argv[1];
+	int num = stoi(test);
+
+	while (num)
+	{
+		t += (num % 2) * d;
+		num = num / 2;
+		d = d * 10; // razryad
+	}
+	cout << t;
+	cout << endl;
+	return 0;
+}
+
